09c2a23dc8352d94a8d19f5b2c7d16c9c0388035
KarlokCheung <georgecheung07@gmail.com>
2021-02-20 23:12:37 +1100

    add about, ninja pages

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci debitis corrupti voluptates maiores error, distinctio suscipit ea, repellendus consequatur reprehenderit numquam impedit sapiente rem doloremque quos laborum consectetur, nobis tenetur?</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam eos ipsa dolor ea rem quae dignissimos, repellat nemo ex nam, iure quibusdam corporis exercitationem, at laudantium a? Laudantium, consectetur eveniet.</p>
+        </div>
+     );
+}
+ 
+export default About;
